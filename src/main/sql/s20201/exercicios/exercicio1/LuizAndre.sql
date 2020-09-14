5ef116734830720d7ac756dc815d5c5193dab496
Lucas Amaral <amarallucas@id.uff.br>
2020-09-14 09:13:17 -0300

    Update LuizAndre.sql

--- src/main/sql/s20201/exercicios/exercicio1/LuizAndre.sql
+++ src/main/sql/s20201/exercicios/exercicio1/LuizAndre.sql
@@ -3,7 +3,9 @@ create table pessoa(
 nome varchar,
 endereco varchar
 );
-insert into pessoa values ('Luiz Andre','end');
+insert into pessoa values ('Lucas Amaral','end');
 insert into pessoa values ('Luiz','end2');
+
+
 
 select * from pessoa;
