dd34b39b86182c61ca593bd30c902da4e9f488d5
Breno Santos <breno.san.reis@gmail.com>
2020-09-14 09:13:49 -0300

    Teste de Commit

--- src/main/sql/s20201/exercicios/exercicio1/LuizAndre.sql
+++ src/main/sql/s20201/exercicios/exercicio1/LuizAndre.sql
@@ -1,9 +1,21 @@
 drop table if exists pessoa  cascade;
+drop table if exists pessoa2 cascade;
+
 create table pessoa(
 nome varchar,
 endereco varchar
 );
+
+create table pessoa2(
+nome varchar,
+endereco varchar
+);
+
 insert into pessoa values ('Luiz Andre','end');
 insert into pessoa values ('Luiz','end2');
 
+insert into pessoa2 values ('Breno Reis', 'end');
+insert into pessoa2 values ('Breno', 'end2');
+
 select * from pessoa;
+select * from pessoa2;
