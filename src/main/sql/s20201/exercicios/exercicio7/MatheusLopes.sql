aa810b03236c32ec7136d0d20c25c2a64a4aeb4c
matheuslcn <71265929+matheuslcn@users.noreply.github.com>
2020-09-23 10:13:21 -0300

    exercicio 7

--- src/main/sql/s20201/exercicios/exercicio7/MatheusLopes.sql
+++ src/main/sql/s20201/exercicios/exercicio7/MatheusLopes.sql
@@ -4,11 +4,11 @@
  * and open the template in the editor.
  */
 /**
- * Author:  matheus
- * Created: 21/09/2020
+ * Author:  mathe
+ * Created: 23/09/2020
  */
 
-drop table if exists matriz cascade;
+drop table if exists matriz;
 
 create table matriz (
     val float[][]
@@ -16,32 +16,27 @@ create table matriz (
 
 insert into matriz values (array[[1,2,3],[4,5,6],[7,8,9]]);
 
-create or replace function detMat (m float[][]) returns float as $$
+
+create or replace function tranporMat (m float[][]) returns float[][] as $$
     declare 
         m_i integer;
         m_j integer;
-        aux float[][];
-        resp float default 1;
-        
+        m_result float[][];
+
     begin
         select array_length(m, 2) into m_i;
         select array_length(m, 1) into m_j;
-        select array_fill(0, array[m_i-1, m_j-1]) into aux;
-            
-        if (m_i!=m_j) then 
-            raise exception 'Não é possível calcular o determinante dessa matriz.';
-        end if;
-        
+        select array_fill(0, array[m_j,m_i]) into m_result;
+
         for i in 1..m_i loop
             for j in 1..m_j loop
-                if (m_i=m_j) then resp = resp * m[i][j];
-                end if;
+                m_result := m[j][i];
             end loop;
         end loop;
-        return resp;
+        return m_result;
     end;
-        
-$$
+$$ 
 language plpgsql;
 
-select detMat(matriz.val) from matriz;
+
+select transporMat (matriz.val) from matriz;
