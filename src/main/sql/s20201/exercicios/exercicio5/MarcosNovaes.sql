a4758c7e1e522144306b086d4326188730e74dac
Marcos Novaes <marcosnovaes@id.uff.br>
2020-09-25 17:42:21 -0300

    correçao tirando criaçoes de tabelas desnecessarias e corrigindo bug

--- src/main/sql/s20201/exercicios/exercicio5/MarcosNovaes.sql
+++ src/main/sql/s20201/exercicios/exercicio5/MarcosNovaes.sql
@@ -1,25 +1,3 @@
-DROP TABLE IF EXISTS matriz1 CASCADE;
-
-CREATE TABLE matriz1
-(
-    content float[][]
-);
-
-DROP TABLE IF EXISTS matriz2 CASCADE;
-
-CREATE TABLE matriz2
-(
-    content float[][]
-);
-
-
-INSERT INTO matriz1 (content) VALUES (ARRAY[[1, 2, 3, 5],[-4, 5, 6, -2],[7, 8, 9, 3], [5, -3, 2, 1]]);
--- determinante == 1828
-
-INSERT INTO matriz2 (content) VALUES (ARRAY[[1, 2],[4, 5],[7, 8]]); 
--- exceção pois nao é matriz quadrada
-
-
 DROP FUNCTION IF EXISTS removeLinhaColunaDaMatriz() CASCADE;
 
 CREATE OR REPLACE FUNCTION removeLinhaColunaDaMatriz (matriz1 float[][], linha integer, coluna integer)
@@ -70,6 +48,7 @@ CREATE OR REPLACE FUNCTION removeLinhaColunaDaMatriz (matriz1 float[][], linha i
 $$
 LANGUAGE PLPGSQL;
 
+
 DROP FUNCTION IF EXISTS determinante() CASCADE;
 
 CREATE OR REPLACE FUNCTION determinante(matriz1 float[][])
@@ -106,6 +85,8 @@ $$
 LANGUAGE PLPGSQL;
 
 
-SELECT determinante(matriz1.content ) FROM matriz1;
+SELECT determinante(ARRAY[[1, 2, 3, 5],[-4, 5, 6, -2],[7, 8, 9, 3], [5, -3, 2, 1]] );
+-- -- determinante == 1828
 
-SELECT determinante(matriz2.content) FROM matriz2;
+-- -- exceção pois nao é matriz quadrada
+SELECT determinante(ARRAY[[1, 2],[4, 5],[7, 8]]);
