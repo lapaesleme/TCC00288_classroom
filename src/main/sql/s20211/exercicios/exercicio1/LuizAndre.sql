223b800fabff27b3c3a5fc25522c0fe3e452bfdd
Luiz Leme <lapaesleme@ic.uff.br>
2021-06-16 09:11:30 -0300

    no message

--- src/main/sql/s20211/exercicios/exercicio1/LuizAndre.sql
+++ src/main/sql/s20211/exercicios/exercicio1/LuizAndre.sql
@@ -1,8 +1,15 @@
+--
+-- Limpesa do BD
+-- Obs.: Executar antes o script create_drop_functions_and_tables.sql
+--
 DO $$ BEGIN
     PERFORM drop_functions();
     PERFORM drop_tables();
 END $$;
 
+--
+-- Criação de dados de teste
+--
 create table pessoa(
 name varchar,
 endereco varchar
@@ -10,5 +17,7 @@ endereco varchar
 
 insert into pessoa values ('nome', 'endereco');
 
-
+--
+-- Programa
+--
 select * from pessoa;
